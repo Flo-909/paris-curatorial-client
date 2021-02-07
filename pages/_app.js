996288383d17baa644eb88297c17e9e3a4f0dcec
Florian Le Rouzic <lerouzic.florian1@gmail.com>
2021-02-07 15:41:24 +0100

    removed the page props of _app.s

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import "../styles/globals.css";
 import Error from "next/error";
 
-function MyApp({ Component }) {
-  return <Component />;
+function MyApp({ Component, pageProps }) {
+  console.log("pageProps in app.js", pageProps);
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
